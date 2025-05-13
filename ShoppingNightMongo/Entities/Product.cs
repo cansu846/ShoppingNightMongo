f2fa1ec911dce85dbff01794827cc4f01d6ddeb8
Cansu Kocaoglu <cansukocaoglu55@gmail.com>
2025-05-13 20:28:57 +0300

    feat: created entites

---
+++ ShoppingNightMongo/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ShoppingNightMongo.Entities
+{
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal ProductPrice { get; set; }
+        public string ImageUrl { get; set; }
+        public bool Status { get; set; }
+        public int StockCount { get; set; }
+    }
+}
