0ac3582d1ef55d5761b9add8ddf002f9df613693
Cansu Kocaoglu <cansukocaoglu55@gmail.com>
2025-05-13 21:45:04 +0300

    feat: added service interface

---
+++ ShoppingNightMongo/Services/CategoryServices/ICategoryService.cs
@@ -0,0 +1,14 @@
+﻿using ShoppingNightMongo.Dtos.CategoryDtos;
+
+namespace ShoppingNightMongo.Services.CategoryServices
+{
+    public interface ICategoryService
+    {
+        Task<List<ResultCategoryDto>> GetAllCategoryAsync(); 
+        Task CreateCategoryAsync(CreateCategoryDto createCategoryDto); 
+        Task DeleteCategoryAsync(string id); 
+        Task UpdateCategoryAsync(UpdateCategoryDto updateCategoryDto); 
+        Task<GetCategoryByIdDto> GetCategoryByIdAsync(string id); 
+
+    }
+}
---
+++ ShoppingNightMongo/Services/ProductServices/ICategoryService.cs
@@ -0,0 +1,14 @@
+﻿using ShoppingNightMongo.Dtos.ProductDtos;
+
+namespace ShoppingNightMongo.Services.ProductServices
+{
+    public interface IProductService
+    {
+        Task<List<ResultProductDto>> GetAllProductAsync(); 
+        Task CreateProductAsync(CreateProductDto createProductDto); 
+        Task DeleteProductAsync(string id); 
+        Task UpdateProductAsync(UpdateProductDto updateProductDto); 
+        Task<GetProductByIdDto> GetProductByIdAsync(string id); 
+
+    }
+}
